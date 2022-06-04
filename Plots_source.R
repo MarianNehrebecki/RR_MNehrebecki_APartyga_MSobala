b87155c0f67d0f4cbae5bed42c11781556696e57
Magdalena Sobala <magda@Magdalenas-MacBook-Air.local>
2022-06-04 16:26:49 +0200

    Updated Report_new.Rmd and Plots_source.R

--- Plots_source.R
+++ Plots_source.R
@@ -3,31 +3,33 @@ library(ggplot2)
 library(tidyverse)
 library(stats)
 
-books <- read.csv('data/book.csv')
+#books <- read.csv('data/book.csv')
 
-View(books)
+#View(books)
 
 options(scipen = 10000)
 
 # Deleting repeated Angels & Deamons (appear twice)
-books <- books[-c(9241),]
+#books <- books[-c(9241),]
+books<-centries_data
 
+### 1. Books with the highest scores across centuries 
+###PROSZE ZMIENIC NA RATINGS -> SCORE TO JEST KOMBINACJA RATINGU I LICZBY VOTES 
+####BEZ SENSU POKAZYWAC TO I LICZBE VOTES, LPIEJ DAC LICZBE VOTES I RATING
 
-### 1. Books with the highest scores across centuries ###
 plot1 <- books %>%
   arrange(desc(scores)) %>%
   head(20) %>%
-  mutate(titles = fct_reorder(titles, scores)) %>%
-  ggplot(., aes(x = titles, y = scores, fill = centries)) +
+  mutate(titles = fct_reorder(titles_new, scores)) %>%
+  ggplot(., aes(x = titles, y = scores)) +
   geom_bar(stat = 'identity') +
-  scale_fill_manual(values =  c('#bebfe6', '#73824a')) + 
+  #scale_fill_manual(values =  c('#bebfe6', '#73824a')) + 
   coord_flip() +
-  labs(y = 'Score', x = 'Book title', title = '20 Books with highest score on Goodreads',
-       fill = 'Century', caption = 'A book?s total score is based on multiple factors, 
-       including the number of people who have voted for it and how highly those voters ranked the book.') +
+  labs(y = 'Score',x="",title="", caption = "A book's total score is based on multiple factors, 
+       including the number of people who have voted for it and how highly those voters ranked the book.") +
   theme(plot.title = element_text(hjust = .5, margin = margin(b = 20)),
         axis.text.x = element_text(margin = margin(b = 10)),
-        axis.text.y = element_text(margin = margin(l = 10)),
+        axis.text.y = element_text(margin = margin(l = 10), face='italic'),
         plot.caption = element_text(size = 8, face = "italic"))
 
 
@@ -35,16 +37,15 @@ plot1 <- books %>%
 plot2<-books %>%
   arrange(desc(times.rated)) %>%
   head(20) %>%
-  mutate(titles = fct_reorder(titles, times.rated)) %>%
-  ggplot(., aes(x = titles, y = times.rated, fill = centries)) +
+  mutate(titles = fct_reorder(titles_new, times.rated)) %>%
+  ggplot(., aes(x = titles, y = times.rated)) +
   geom_bar(stat = 'identity') +
   scale_fill_manual(values =  c('#ba8b32', '#bebfe6', '#73824a')) + 
   coord_flip() +
-  labs(y = 'Number of times rated', x = 'Book title', title = 'Top 20 most rated books',
-       fill = 'Century') +
+  labs(y = 'Number of times rated',x="",title="") +
   theme(plot.title = element_text(hjust = .5, margin = margin(b = 20)),
         axis.text.x = element_text(margin = margin(b = 10)),
-        axis.text.y = element_text(margin = margin(l = 10)))
+        axis.text.y = element_text(margin = margin(l = 10), face='italic'))
 
 books %>%
   filter(titles == 'Angels & Demons (Robert Langdon, #1)' & centries == '20th')
@@ -62,14 +63,13 @@ plot3<-books %>%
   filter(authors != 'Anonymous' & authors != 'Unknown') %>%
   head(20) %>%
   mutate(authors = fct_reorder(authors, books_written)) %>%
-  ggplot(., aes(x = authors, y = books_written, fill = centries)) +
+  ggplot(., aes(x = authors, y = books_written)) +
   geom_bar(stat = 'identity') +
   scale_fill_manual(values =  c('#687796', '#ba8b32', '#bebfe6', '#73824a')) + 
   coord_flip() +
-  labs(y = 'Number of books written', x = 'Author', title = 'Top 20 prolific authors',
-       fill = 'Century') +
+  labs(y = 'Number of books written',x="",title="") +
   theme(plot.title = element_text(hjust = .5, margin = margin(b = 20)),
         axis.text.x = element_text(margin = margin(b = 10)),
-        axis.text.y = element_text(margin = margin(l = 10)))
+        axis.text.y = element_text(margin = margin(l = 10),face='italic'))
 
 
